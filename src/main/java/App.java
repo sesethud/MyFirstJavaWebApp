5c499fc66132fa3b760022b0d0772cc16f65d904
sesethud <dsesethu60@gmail.com>
2020-08-07 13:07:59 +0200

    After adding radion buttons

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,13 +1,10 @@
 import static spark.Spark.*;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
-
 public class App {
     static int getHerokuAssignedPort() {
         ProcessBuilder processBuilder = new ProcessBuilder();
@@ -17,32 +14,22 @@ public class App {
         return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
     public static void main(String []args){
-
         port(getHerokuAssignedPort());
-
         // root is 'src/main/resources', so put files in 'src/main/resources/public'
         staticFiles.location("/public"); // Static files
-
       //  List <String> names= new ArrayList<>();
         Map<String, Integer> users = new HashMap<>();
       //  get("/hello", (req, res) -> "Hello World");
         get("/greet", (req, res) -> "Hello!");
-
         get("/hello", (req, res) -> {
-
             Map<String, Object> map = new HashMap<>();
             return new ModelAndView(map, "hello.handlebars");
-
         }, new HandlebarsTemplateEngine());
-
         post("/hello", (req, res) -> {
-
             Map<String, Object> map = new HashMap<>();
-            String username=req.queryParams("username");
+            String username = req.queryParams("username");
             // create the greeting message
-
             //***
-            String greeting = "Hello, " + username;
            // names.add(username);
             if(users.containsKey(username)){
                 users.put(username, users.get(username) +1); // if username exist
@@ -50,17 +37,24 @@ public class App {
             else{
                 users.put(username, 1);
             }
+            // Code for radio buttons
+            String language=req.queryParams("Language");
+            String emptyString="";
+            switch (language){
+                case "Xhosa":
+                    emptyString = "Mholo "+username;
+                    break;
+                case "English":
+                    emptyString ="Hello "+ username;break;
+                case "Zulu": emptyString = "Sawubona "+username; break;
+                //default:break;
+            }
 //            map.put("users",names);
             //**
-
             // put it in the map which is passed to the template - the value will be merged into the template
-            map.put("greeting", greeting);
+            map.put("greeting", emptyString);
             map.put("users", users);
-
             return new ModelAndView(map, "hello.handlebars");
-
         }, new HandlebarsTemplateEngine());
-
-
     }
 }
