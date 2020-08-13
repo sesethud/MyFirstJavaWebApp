e6848d567f4c266f3f4c698539bcf438acafaeed
sesethud <dsesethu60@gmail.com>
2020-08-13 10:46:22 +0200

    initial database commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,6 +3,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import org.jdbi.v3.core.Handle;
+import org.jdbi.v3.core.Jdbi;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 public class App {
@@ -14,31 +17,104 @@ public class App {
         return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
     public static void main(String []args){
+
+        String dbDiskURL = "jdbc:h2:file:./greetdb";
+//        String dbMemoryURL = "jdbc:h2:mem:greetdb";
+
+        Jdbi jdbi = Jdbi.create(dbDiskURL, "sa", "");
+
+// get a handle to the database
+        Handle handle = jdbi.open();
+
+
+// create the table if needed
+        handle.execute("create table if not exists greet ( id integer identity, name varchar(50), counter int )");
+
+
         port(getHerokuAssignedPort());
         // root is 'src/main/resources', so put files in 'src/main/resources/public'
+
         staticFiles.location("/public"); // Static files
-      //  List <String> names= new ArrayList<>();
+       // List <String> names= new ArrayList<>();
         Map<String, Integer> users = new HashMap<>();
       //  get("/hello", (req, res) -> "Hello World");
-        get("/greet", (req, res) -> "Hello!");
+        //get("/greet", (req, res) -> "Hello!");
         get("/hello", (req, res) -> {
             Map<String, Object> map = new HashMap<>();
             return new ModelAndView(map, "hello.handlebars");
         }, new HandlebarsTemplateEngine());
+
+
+
+        get("/",(req,res) ->{
+
+            //get all from the database
+            List<String> usersFromDatabase = handle.createQuery("select name from greet")
+                    .mapTo(String.class)
+                    .list();
+
+            Map<String,Object> map =new HashMap<>();
+
+            map.put("users",usersFromDatabase);
+            map.put("counter",usersFromDatabase.size());
+
+            return new ModelAndView(map,"hello.handlebars");
+
+        },new HandlebarsTemplateEngine());
+
+
+        post("/greet",(req,res)->{
+            String username=req.queryParams("username");
+            String greeting="";
+            if(!username.trim().equals("")){
+                greeting=String.format("Hello %s",username);
+
+
+                //add users to the database
+                handle.execute("insert into greet (name) values (?)",username);
+            }
+
+            List<String> localUsers=handle.createQuery("select name from greet")
+                    .mapTo(String.class)
+                    .list();
+            Map<String, Object> map = new HashMap<>();
+//            handle.execute("insert into greet (name) values(?)",userList);
+
+            map.put("users",localUsers);    // here
+            map.put("greeting",greeting);
+            return new ModelAndView(map, "hello.handlebars");
+        },new HandlebarsTemplateEngine());
+
+
+
         post("/hello", (req, res) -> {
+
+           // Map<String, Integer> users = new HashMap<>();
             Map<String, Object> map = new HashMap<>();
             String username = req.queryParams("username");
             // create the greeting message
-            //***
+            // ***
            // names.add(username);
-            if(users.containsKey(username)){
-                users.put(username, users.get(username) +1); // if username exist
+            if(!username.trim().equals("")  ){//&& users.containsKey(username)){
+                //users.put(username, users.get(username) +1); // if username exist
+                //handle.execute("insert into greet (name) values (?)",username);
+                //
+
+                int count = handle.select("select count(*) from greet where name = ?", username)
+                        .mapTo(int.class)
+                        .findOnly();
+                if (count==0){
+                    handle.execute("insert into greet (name) values (?)",username);
+                }
+
             }
-            else{
-                users.put(username, 1);
-            }
-            // Code for radio buttons
-            String language=req.queryParams("Language");
+
+           else{
+//                //users.put(username, 1);
+                handle.execute("insert into greet (name) values (?)",username);
+          }
+              // Code for radio buttons
+          /*  String language=req.queryParams("Language");
             String emptyString="";
             switch (language){
                 case "Xhosa":
@@ -49,12 +125,25 @@ public class App {
                 case "Zulu": emptyString = "Sawubona "+username; break;
                 //default:break;
             }
-//            map.put("users",names);
+//          map.put("users",names);
             //**
             // put it in the map which is passed to the template - the value will be merged into the template
-            map.put("greeting", emptyString);
-            map.put("users", users);
+            map.put("greeting", emptyString);*/
+
+            List<String> videoUsers = handle.createQuery("select name from greet")
+                    .mapTo(String.class)
+                    .list();
+
+
+            map.put("users", videoUsers);
+            map.put("size",videoUsers.size());
+
+
             return new ModelAndView(map, "hello.handlebars");
         }, new HandlebarsTemplateEngine());
     }
+
+
+
+
 }
---
+++ src/main/java/Greeting.java
@@ -0,0 +1,20 @@
+public class Greeting {
+    String name;
+    int counter;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCounter(int count) {
+        this.counter = count;
+    }
+
+    public int getCounter() {
+        return counter;
+    }
+}
